c70069b23068460906e73e74addb6416b347ff6d
raydN development <39228636+raydNDev@users.noreply.github.com>
2019-01-07 03:01:59 +0100

    Add files via upload

---
+++ client_packages/client.js
@@ -0,0 +1,21 @@
+/* (c) Xendom Rayden / raydN development */
+
+var cef = mp.browsers.new("package://assets/registerLogin.html");
+mp.gui.cursor.visible = true;
+
+/* Trigger */
+mp.events.add("registerLogin.client",function(name,password,type) {
+	if(name.length >= 3 && password.length >= 5) {
+		mp.events.callRemote("registerLogin.server",name,password,type);
+	}
+	else
+	{
+		mp.game.graphics.notify("Die Angaben sind ungültig!");
+	};
+});
+
+/* Destroy Register / Login */
+mp.events.add("registerLogin.destroy",function() {
+	cef.destroy();
+	mp.gui.cursor.visible = false;
+});
